cc247f3619ef47c7ac5f60f72e15a12dd3f68e4f
Rikuto Iwashita <02rikuto18@gmail.com>
2025-06-14 13:05:12 +0900

    クイズデータを作成しました

---
+++ src/data/quiz.js
@@ -0,0 +1,25 @@
+const quizData = [
+    {
+        question: "Question 1",
+        options: ["select 1", "select 2", "select 3", "select 4"],
+        answerIndex: 0
+    }, {
+        question: "Question 2",
+        options: ["select 1", "select 2", "select 3", "select 4"],
+        answerIndex: 1
+    }, {
+        question: "Question 3",
+        options: ["select 1", "select 2", "select 3", "select 4"],
+        answerIndex: 2
+    }, {
+        question: "Question 4",
+        options: ["select 1", "select 2", "select 3", "select 4"],
+        answerIndex: 3
+    }, {
+        question: "Question 5",
+        options: ["select 1", "select 2", "select 3", "select 4"],
+        answerIndex: 1
+    }
+]
+
+export default quizData
