a63caf6c7bfad90e864c2938f8b0b8b51d16eff4
Rikuto Iwashita <02rikuto18@gmail.com>
2025-06-14 16:43:33 +0900

    4つの<Button>をmap関数で表しました。

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import styles from "./Button.module.css";
 
-export default function Button({children}) {
-  return (
-    <button className={styles.button} type='button'>
-        <span className={styles.buttonInner}>{children}</span>
-    </button>
-  )
+export default function Button({ children, onClick }) {
+    return (
+        <button className={styles.button} type='button' onClick={onClick}>
+            <span className={styles.buttonInner}>{children}</span>
+        </button>
+    )
 }
